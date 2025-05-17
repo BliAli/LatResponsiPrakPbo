41a2574063cf6e7e7078c4b051371159b8129b7a
BliAli <aliaddamawi@gmail.com>
2025-05-17 14:43:43 +0700

    Create design dan koneksi ke database perpustakaan

--- PerpustakaanUmum/src/Koneksi/Conncector.java
+++ PerpustakaanUmum/src/Koneksi/Conncector.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Koneksi;
-
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 /**
  *
  * @author aliad
  */
 public class Conncector {
+    static Connection conn;
     
+    public static Connection connection(){
+        if(conn == null){
+           MysqlDataSource database = new MysqlDataSource();
+           database.setDatabaseName("perpustakaan_umum");
+           database.setUser("root");
+           database.setPassword("");
+           
+            try {
+               conn = database.getConnection();
+                System.out.println("Koneksi Database Perpustakaan Umum Berhasil");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                System.out.println("Koneksi Database Perpustakaan Umum Gagal");
+            }
+        }
+        return conn;
+    }
 }
